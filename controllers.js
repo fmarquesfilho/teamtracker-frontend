043b35d43a2563d7812a715f5038bdeb8a93105e
Luiz Rogerio <luizrogeriocn@gmail.com>
2014-08-31 01:35:37 -0300

    Add sorting by date

--- controllers.js
+++ controllers.js
@@ -11,6 +11,14 @@ ttApp.controller('FeedCtrl', function ($scope, $http) {
     .success(function(data) {
         console.log("received:"+ data);
         $scope.notifications = data.notifications;
+        $scope.notifications.sort(function(a, b){
+         var date1=(a.created_at ||a.commits[0].timestamp), date2=(b.created_at||b.commits[0].timestamp)
+         if (date1 < date2) //sort string ascending
+          return -1 
+         if (date1 > date2)
+          return 1
+         return 0 //default return value (no sorting)
+        })
     })
     .error(function(data) {
         console.log("Error: " + data);
