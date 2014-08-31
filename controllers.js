66f99669e359c2ff1bc339192ccf083e0e1559c0
Luiz Rogerio <luizrogeriocn@gmail.com>
2014-08-31 01:39:19 -0300

    Change sorting order

--- controllers.js
+++ controllers.js
@@ -13,9 +13,9 @@ ttApp.controller('FeedCtrl', function ($scope, $http) {
         $scope.notifications = data.notifications;
         $scope.notifications.sort(function(a, b){
          var date1=(a.created_at ||a.commits[0].timestamp), date2=(b.created_at||b.commits[0].timestamp)
-         if (date1 < date2) //sort string ascending
+         if (date1 > date2) //sort string ascending
           return -1 
-         if (date1 > date2)
+         if (date1 < date2)
           return 1
          return 0 //default return value (no sorting)
         })
