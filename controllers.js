2d75840ca11005e82ebf7c36a17400928de85cac
Luiz Rogerio <luizrogeriocn@gmail.com>
2014-08-31 02:14:07 -0300

    Add partial for pushes with more than one commit

--- controllers.js
+++ controllers.js
@@ -11,13 +11,21 @@ ttApp.controller('FeedCtrl', function ($scope, $http) {
     .success(function(data) {
         console.log("received:"+ data);
         $scope.notifications = data.notifications;
+
+        for(var i = 0; i < $scope.notifications.length; i++){
+            if(($scope.notifications[i].commits !== undefined) && ($scope.notifications[i].commits.length < 2)){
+                $scope.notifications[i]['type'] = 'commit';
+                console.log($scope.notifications[i]['type']);
+            }
+        }
+
         $scope.notifications.sort(function(a, b){
          var date1=(a.created_at ||a.commits[0].timestamp), date2=(b.created_at||b.commits[0].timestamp)
-         if (date1 > date2) //sort string ascending
+         if (date1 > date2) 
           return -1 
          if (date1 < date2)
           return 1
-         return 0 //default return value (no sorting)
+         return 0
         })
     })
     .error(function(data) {
