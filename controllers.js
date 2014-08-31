513613742dcfa5fb4aa877a4adcaab19a694bbc2
Luiz Rogerio <luizrogeriocn@gmail.com>
2014-08-31 01:11:17 -0300

    Add partials for issue and push notifications

--- controllers.js
+++ controllers.js
@@ -2,14 +2,15 @@ var ttApp = angular.module('ttApp', []);
 
 ttApp.controller('FeedCtrl', function ($scope, $http) {
 
-  $http({
+    $scope.notifications = [];    
+
+    $http({
         method: 'GET',
         url: 'http://107.170.231.15/public/data/commits.json'
     })
     .success(function(data) {
         console.log("received:"+ data);
-        $scope.commits = data.notifications.commits;
-        $scope.issues = data.notifications.issues;
+        $scope.notifications = data.notifications;
     })
     .error(function(data) {
         console.log("Error: " + data);
