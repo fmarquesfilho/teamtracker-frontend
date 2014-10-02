deaff9c0afe5725d7c15b9093989d833648100e9
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2014-10-02 14:21:23 +0000

    Basic single push card integration

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,19 +5,20 @@ ttApp.controller('FeedCtrl', function ($scope, $http) {
   $http.get('http://tt.diredevs.com/feed/1')
   .success(function(data) {
     $scope.notifications = data;
-    console.log(data);
   })
   .error(function(data) {
     console.log('Error: ' + data);
   });
 
   $scope.getCard = function(notification) {
-    console.log(notification);
     
     if (notification.event_action)
       return 'cards/slack.html';
-    else if (notification.event_name == "push")
-      return 'cards/push.html';
+    else if (notification.event_name == "push") {
+      if (notification.object.commits.length == 1)
+        return 'cards/single_push.html';
+      else return 'cards/mult_push.html';
+    }
     else
       return "cards/todo.html";
   };
