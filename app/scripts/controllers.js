ff27adf8f874d1ff39cb949544d26bebc23e8650
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2014-10-02 03:26:54 +0000

    Prepare angular for integration

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,27 +1,34 @@
 var ttApp = angular.module('ttApp', []);
 
 ttApp.controller('FeedCtrl', function ($scope, $http) {
-  $scope.notifications = [
-    {'author': 'Jeff Winger',
-     'message': 'Nah, I\'ll do it later.',
-     'date': '12/08/2014'},
-    {'author': 'Abed Nadir',
-     'message': 'Cool. Cool cool code.',
-     'date': '12/08/2014'},
-    {'author': 'Troy Barnes',
-     'message': 'Add layers. SO MANY LAYERS',
-     'date': '12/08/2014'},
-  ];
+  
+  $http.get('http://tt.diredevs.com/feed/1')
+  .success(function(data) {
+    $scope.notifications = data;
+    console.log(data);
+  })
+  .error(function(data) {
+    console.log('Error: ' + data);
+  });
 
-  // $http.get({
-  //       method: 'GET',
-  //       url: 'http://107.170.231.15/public/data/commits.json'
-  //   })
-  //   .success(function(data) {
-  //       console.log("received:"+ data);
-  //   })
-  //   .error(function(data) {
-  //       console.log("Error: " + data);
-  //   });
-
+  $scope.getCard = function(notification) {
+    console.log(notification);
+    
+    if (notification.event_action)
+      return 'cards/slack.html';
+    else if (notification.event_name == "push")
+      return 'cards/push.html';
+    else
+      return "cards/todo.html";
+  };
+})
+.directive('card', function($parse) {
+  return {
+    restrict: 'A',
+    link: function ($scope, element, $attr) {
+      var data = JSON.parse($attr.data);
+      
+      element.html(data.event_action);
+    }
+  };
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,7 +15,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License
  *
- */
+ 
 (function () {
   'use strict';
 
@@ -48,3 +48,4 @@
     }
   });
 })();
+*/
